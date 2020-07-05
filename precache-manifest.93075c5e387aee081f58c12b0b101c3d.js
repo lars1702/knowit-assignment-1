93f01f9b8346b084f840c12073b8ee46bd4b2fa3
Lars1702 <Lars1702@gmail.com>
2020-07-05 15:40:38 +0200

    Updates

---
+++ precache-manifest.93075c5e387aee081f58c12b0b101c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e0c6b8f2e817e4cfb731f6b20d4d53",
+    "url": "/knowit-assignment-1/index.html"
+  },
+  {
+    "revision": "db91fc0cc0da2e92ec9c",
+    "url": "/knowit-assignment-1/static/css/main.cedcec0d.chunk.css"
+  },
+  {
+    "revision": "c00b04304a3761b47aaa",
+    "url": "/knowit-assignment-1/static/js/2.7d190269.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/knowit-assignment-1/static/js/2.7d190269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db91fc0cc0da2e92ec9c",
+    "url": "/knowit-assignment-1/static/js/main.444be078.chunk.js"
+  },
+  {
+    "revision": "142c04fc264b8786d664",
+    "url": "/knowit-assignment-1/static/js/runtime-main.2a108cf6.js"
+  }
+]);
